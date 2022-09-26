fccdc7dd4d23c38055026d5a2317bc4503a37757
azizp128 <azizprabowo128@gmail.com>
2022-09-26 12:05:59 +0700

    third commit

--- script.js
+++ script.js
@@ -3,15 +3,19 @@ const rating = document.querySelectorAll(".rating");
 const submitBtn = document.querySelector(".card__button");
 const cardOne = document.querySelector(".card-1");
 const cardTwo = document.querySelector(".card-2");
+const cardStateRate = document.querySelector(".card__state__rate");
 
 cardRating.addEventListener("click", function (e) {
-  rating.forEach((rating) => {
-    rating.className = "rating";
+  rating.forEach((r) => {
+    r.className = "rating";
   });
 
   if (e.target.classList == "rating") {
     e.target.classList.add("active");
   }
+
+  cardStateRate.innerHTML = e.target.innerHTML;
+  cardStateRate.style.fontSize = "0.7rem";
 });
 
 submitBtn.addEventListener("click", (e) => {
